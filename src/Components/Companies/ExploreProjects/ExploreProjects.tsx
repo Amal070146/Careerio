98a224963e6c68c5592db9143c0c3a8dbe16cea0
Amal C P <acp070146@gmail.com>
2023-11-19 00:57:29 +0530

    profile page structure

--- src/Components/Companies/ExploreProjects/ExploreProjects.tsx
+++ src/Components/Companies/ExploreProjects/ExploreProjects.tsx
@@ -1,10 +1,11 @@
 import styles from "./ExploreProjects.module.css";
 import ey from "../PostGigs/assets/ey.png";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { DrumsIcon } from "../PostGigs/utils/commom";
 
-type Props = {};
 
-export const ExploreProjects = (_props: Props) => {
+
+export const ExploreProjects = () => {
   const data = [
     {
       image: ey,
@@ -78,33 +79,72 @@ export const ExploreProjects = (_props: Props) => {
     },
     // ... more data items if needed
   ];
-  const navigate = useNavigate()
+
   return (
     <div className={styles.ExploreProjectsWrapper}>
-      {data.map(({ name, title, description, stacks, collabs }, index) => (
-        <div key={index} className={styles.IndividualWrapper} onClick={() => navigate(`/project/${name}`)}>
-          <div className={styles.FirstSec}>
-            <h1>{name}</h1>
-            <p>projects</p>
-          </div>
-          <div className={styles.SecondDiv}>
-            {stacks.map((stack, stackIndex) => (
-              <p key={stackIndex}>{stack}</p>
-            ))}
-          </div>
-          <div className={styles.ThirdDiv}>
-            <div>
-              <h2>{title}</h2>
-              <p>{description}</p>
-            </div>
-            <div className={styles.collaborators}>
-              {collabs.map((collab) => (
-                <img src={collab} alt="" />
-              ))}
-            </div>
-          </div>
+      <div className={styles.HeaderSection2}>
+        <div>
+          {" "}
+          <DrumsIcon colors="#fff" width={150} height={150} />
         </div>
+        <div className={styles.ContentSession}>
+          <h1>Get your work done with Projects!</h1>
+          <p>
+            Are you a company seeking the top talents? Look no further! We are
+            your trusted partner for gig work postings, connecting you with the
+            best freelancers, contractors, and gig workers in the industry.
+            Partner with us today and experience the difference in gig work
+            postings.
+          </p>
+          <Link to="/formpostgig">Post a Project!</Link>
+        </div>
+      </div>
+      {data.map(({ name, title, description, stacks, collabs }) => (
+       <CardProjects name={name} title={title} description={description} stacks={stacks} collabs={collabs} />
       ))}
     </div>
   );
 };
+
+
+
+
+type Props = {
+  name?: string;
+  title?: string;
+  description?: string;
+  stacks?:string[];
+  collabs?: string[];
+
+}
+
+export const CardProjects = (props: Props) => {
+    const navigate = useNavigate();
+  return (
+    <div
+      className={styles.IndividualWrapper}
+      onClick={() => navigate(`/project/${props.name}`)}
+    >
+      <div className={styles.FirstSec}>
+        <h1>{props.name}</h1>
+        <p>projects</p>
+      </div>
+      <div className={styles.SecondDiv}>
+        {props.stacks?.map((stack, stackIndex) => (
+          <p key={stackIndex}>{stack}</p>
+        ))}
+      </div>
+      <div className={styles.ThirdDiv}>
+        <div>
+          <h2>{props.title}</h2>
+          <p>{props.description}</p>
+        </div>
+        <div className={styles.collaborators}>
+          {props.collabs?.map((collab) => (
+            <img src={collab} alt="" />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
